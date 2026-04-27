68ab4539b905a381c1033daac229f12810ae122f
Steve <smuturi795@gmail.com>
2026-04-27 15:56:59 +0300

    Invalid test cases
    Worked on the saving with no first name and also saving with no last name cases. Both of which are invalid

--- src/main/java/pages/PIMPage.java
+++ src/main/java/pages/PIMPage.java
@@ -4,6 +4,7 @@ import base.BasePage;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
+import net.datafaker.Faker;
 
 public class PIMPage extends BasePage {
     private final Locator PIMHeader;
@@ -13,19 +14,21 @@ public class PIMPage extends BasePage {
     private final Locator strEmployeeId;
     private final Locator btnSave;
     private final Locator employeeListLink;
+    private final Locator errorMessage;
     private final Locator btnAddEmployee;
+
+    Faker faker=new Faker();
 
     public PIMPage(Page page) {
         super(page);
         this.PIMHeader=page.getByRole(AriaRole.HEADING,new Page.GetByRoleOptions().setName("PIM"));
-
-        //this.PIMHeader= page.locator(".oxd-topbar-header-title");
         this.txtFirstName =page.locator("input[name='firstName']");
         this.txtMiddleName = page.getByPlaceholder("Middle Name");
         this.txtLastName = page.getByPlaceholder("Last Name");
         this.strEmployeeId = page.locator("//div[@class='oxd-input-group oxd-input-field-bottom-space']//div//input[@class='oxd-input oxd-input--active']");
         this.btnSave = page.locator("button[type='submit']");
         this.employeeListLink=page.getByRole(AriaRole.LINK,new Page.GetByRoleOptions().setName("Employee List"));
+        this.errorMessage=page.getByText("Required", new Page.GetByTextOptions().setExact(true));
         this.btnAddEmployee=page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add"));
     }
 
@@ -58,6 +61,13 @@ public class PIMPage extends BasePage {
         btnSave.click();
     }
 
+    public Locator getRequiredError() {
+        return page.getByText(
+                "Required",
+                new Page.GetByTextOptions().setExact(true)
+        );
+    }
+
     public void saveEmployeeDetails(String firstName, String middleName, String lastName,String id){
         clickAddButton();
         enterFirstName(firstName);
@@ -66,5 +76,23 @@ public class PIMPage extends BasePage {
         enterEmployeeID(id);
         clickSaveButton();
        // page.pause();
+    }
+
+
+
+
+    public void saveEmployeeDetails(String firstName, String middleName, String id){
+        clickAddButton();
+        enterFirstName(firstName);
+        enterMiddleName(middleName);
+        enterEmployeeID(id);
+        clickSaveButton();
+    }
+
+    public void saveEmployeeDetails(String middleName, String id){
+        clickAddButton();
+        enterMiddleName(middleName);
+        enterEmployeeID(id);
+        clickSaveButton();
     }
 }
--- src/test/java/tests/PIMTest.java
+++ src/test/java/tests/PIMTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import base.BaseTest;
 import com.microsoft.playwright.assertions.LocatorAssertions;
+import net.datafaker.Faker;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.DashboardPage;
@@ -13,16 +14,62 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 
 public class PIMTest extends BaseTest {
     ConfigReader configReader=ConfigReader.getInstance();
+    Faker faker=new Faker();
 
     @Test
-    public void saveValidEmployeeDetails(){
+    public void saveEmployeeDetails_success(){
         LoginPage loginPage=new LoginPage(page);
         loginPage.userLogin(configReader.getProperty("admin.username"),configReader.getProperty("admin.password"));
 
         DashboardPage dashboardPage=new DashboardPage(page);
         PIMPage pimPage=dashboardPage.navigateToPIM();
-        pimPage.saveEmployeeDetails(configReader.getProperty("employee.firstname"), configReader.getProperty("employee.middlename"), configReader.getProperty("employee.lastname"), configReader.getProperty("employee.id"));
 
-        assertThat(page.getByText(configReader.getProperty("employee.firstname"))).isVisible(new LocatorAssertions.IsVisibleOptions().setTimeout(10000));;
+        String firstName=faker.name().firstName();
+        String middleName=faker.name().firstName();
+        String lastName=faker.name().lastName();
+        String id=faker.number().digits(5);
+
+        pimPage.saveEmployeeDetails(firstName,middleName,lastName,id);
+
+        //assertThat(page.getByText(configReader.getProperty("employee.firstname"))).isVisible(new LocatorAssertions.IsVisibleOptions().setTimeout(10000));;
     }
+
+    @Test
+    public void saveEmployeeDetails_noLastName(){
+        LoginPage loginPage=new LoginPage(page);
+        loginPage.userLogin(configReader.getProperty("admin.username"),configReader.getProperty("admin.password"));
+
+        DashboardPage dashboardPage=new DashboardPage(page);
+        PIMPage pimPage=dashboardPage.navigateToPIM();
+
+        String firstName=faker.name().firstName();
+        String middleName=faker.name().firstName();
+        String id=faker.number().digits(5);
+
+        pimPage.saveEmployeeDetails(firstName,middleName,id);
+
+        System.out.println("sdfsdf");
+        System.out.println(pimPage.getRequiredError());
+
+        assertThat(pimPage.getRequiredError()).isVisible();
+    }
+
+    @Test
+    public void saveEmployeeDetails_noFirstAndLastNames(){
+        LoginPage loginPage=new LoginPage(page);
+        loginPage.userLogin(configReader.getProperty("admin.username"),configReader.getProperty("admin.password"));
+
+        DashboardPage dashboardPage=new DashboardPage(page);
+        PIMPage pimPage=dashboardPage.navigateToPIM();
+
+        String middleName=faker.name().firstName();
+        String id=faker.number().digits(5);
+
+        pimPage.saveEmployeeDetails(middleName,id);
+
+        assertThat(pimPage.getRequiredError()).hasCount(2);
+    }
+
+    public void saveEmployeeDetails_duplicateId(){}
+
 }
