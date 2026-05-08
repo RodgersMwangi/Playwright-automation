7c5465a7ef95e2c9f6e1064dfe0b1bf9ce44fbb8
rowlrowl <rdgrsmwangi@gmail.com>
2026-05-08 11:14:43 +0300

    Centralized DataFaker data into DataFaker.java

--- src/main/java/util/DataFaker.java
+++ src/main/java/util/DataFaker.java
@@ -4,12 +4,17 @@ import net.datafaker.Faker;
 public class DataFaker {
     public static final Faker FAKER = new Faker();
 
+    public static String firstName=DataFaker.FAKER.name().firstName();
+    public static String lastName=DataFaker.FAKER.name().lastName();
+    public static String name = firstName + " " + lastName;
+    public static String id = DataFaker.FAKER.number().digits(6);
+    public static String userName= DataFaker.FAKER.regexify("[a-zA-Z]{5,10}");
+    public static String userPassword=DataFaker.FAKER.regexify("[A-Z]{1}[a-z]{5}[0-9]{2}[@#$%]{1}");
+
+
     /*
     * How to use
     *
-    * String name = DataFaker.FAKER.name().firstName();
-      System.out.println(name);
-      * String password = DataFaker.FAKER.regexify("[A-Z]{1}[a-z]{5}[0-9]{2}[@#$%]{1}");
-      * System.out.println(password);
+    * String employeeFirstName=DataFaker.firstName;
     * */
 }
