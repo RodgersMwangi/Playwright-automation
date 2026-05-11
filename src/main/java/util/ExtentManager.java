113cb163d66c86c73fca919743c0b751fa46cf5f
Steve <smuturi795@gmail.com>
2026-05-11 18:23:57 +0300

    Modified the report to reproduce on each run instead of using a timestamp generated report

--- src/main/java/util/ExtentManager.java
+++ src/main/java/util/ExtentManager.java
@@ -9,10 +9,7 @@ public class ExtentManager {
 
     public static ExtentReports getInstance(){
         if (extentReports==null){
-            //Prevents overwriting the reports file on each run,each run now generates its own report
-            String path = "test-output/ExtentReport_" + System.currentTimeMillis() + ".html";
-            ExtentSparkReporter sparkReporter = new ExtentSparkReporter(path);
-            //ExtentSparkReporter sparkReporter=new ExtentSparkReporter("test-output/ExtentReport.html");
+            ExtentSparkReporter sparkReporter=new ExtentSparkReporter("test-output/ExtentReport.html");
 
             sparkReporter.config().setReportName("Regression test results");
             sparkReporter.config().setDocumentTitle("Test execution report");
